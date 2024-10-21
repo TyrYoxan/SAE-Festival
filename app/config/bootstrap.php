3e0d0ce8c1115381db76f610cdb6c2fb969b7912
tyryoxan <clem.bri25@gmail.com>
2024-10-21 10:24:22 +0200

    Init project

---
+++ app/config/bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+use DI\ContainerBuilder;
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use toubeelib\application\middlewares\Cors;
+
+
+
+$builder = new ContainerBuilder();
+$builder->addDefinitions(__DIR__ . '/settings.php' );
+$builder->addDefinitions(__DIR__ . '/dependencies.php');
+
+$c=$builder->build();
+$app = AppFactory::createFromContainer($c);
+
+
+$app->addBodyParsingMiddleware();
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware($c->get('displayErrorDetails'), false, false)
+    ->getDefaultErrorHandler()
+    ->forceContentType('application/json');
+
+
+$app = (require_once __DIR__ . '/routes.php')($app);
+$routeParser = $app->getRouteCollector()->getRouteParser();
+
+
+return $app;
