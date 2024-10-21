05a30b6837624ab7d46f7b6356eb87e97d863b46
tyryoxan <clem.bri25@gmail.com>
2024-10-21 14:11:57 +0200

    Début route

---
+++ app/config/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+return  [
+
+    'displayErrorDetails' => true,
+    'logs.dir' => __DIR__ . '/../var/logs',
+    'db' => [
+        'driver' => 'mysql',
+        'host' => 'festival.db',
+        'database' => 'festival.db',
+        'username' => 'root',
+        'password' => 'root',
+    ]
+
+    ] ;
